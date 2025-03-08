2602750860fa9895c8be5ddb014b518380978897
YASMINE NASCIMENTO LAMARK <yasmine.nlamark@senacsp.edu.br>
2025-03-07 22:21:53 -0300

    criamos uma classe

---
+++ login/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    internal class Usuario
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
