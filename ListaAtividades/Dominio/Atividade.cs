3917859bc7e2694903f1042c2f3968d31c9772e1
YASMINE NASCIMENTO LAMARK <yasmine.nlamark@senacsp.edu.br>
2025-04-14 22:26:08 -0300

    feat criando dominio

--- ListaAtividades/Dominio/Atividade.cs
+++ ListaAtividades/Dominio/Atividade.cs
@@ -11,5 +11,35 @@ namespace ListaAtividades.Dominio
         public int Id { get; set; }
         public string Titulo { get; set; }
         public Situacao Situacao { get; set; }
+
+        public void Criar()
+        {
+
+        }
+        public void AtualizarSituacao()
+        {
+
+        }
+
+        public Atividade BuscarAtividadeAndamento()
+        {
+            return new Atividade();
+        }
+
+        public List<Atividade> ListarAtividadesPendentes()
+        {
+            return [];
+        }
+
+        private bool ValidarTitulo()
+        {
+            return string.IsNullOrEmpty(Titulo);
+        }
+
+        private Situacao BuscarProximaSituacao()
+        {
+            return Situacao.Concluido;
+        }
     }
 }
+
