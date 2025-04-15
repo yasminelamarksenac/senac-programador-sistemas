1afbc06d751334d1f1a1087b80539f9ab8711995
YASMINE NASCIMENTO LAMARK <yasmine.nlamark@senacsp.edu.br>
2025-04-14 22:20:06 -0300

    criando projeto

---
+++ ListaAtividades/BancoDeDados/DataBase.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ListaAtividades.BancoDeDados
+{
+    internal class DataBase
+    {
+
+        private static readonly string ConnectionString = "datasource=localhost;username=root;password=;database=senac;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+
+}
---
+++ ListaAtividades/Dominio/Atividade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAtividades.Dominio
+{
+    internal class Atividade
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public Situacao Situacao { get; set; }
+    }
+}
